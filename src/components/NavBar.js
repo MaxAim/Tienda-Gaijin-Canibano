530b8451a4d008f1628037e5897bc790e672c71a
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-06-13 22:22:55 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <header className="NavBar">
+        <div style={{ padding: "0 2% 0 2%" }}>
+          <h1>
+            Tienda <b style={{ fontSize: "2rem" }}>⬤</b> Gaijin
+          </h1>
+        </div>
+        <a>Todos</a>
+        <a>Comida</a>
+        <a>Ropa</a>
+        <a>Figuras</a>
+        <a>Otros</a>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
