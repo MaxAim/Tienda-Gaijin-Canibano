b4a6afa56e3b3abdb6301c9885a433316cc4a675
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-06-20 17:43:12 -0300

    Delete ItemListContainer.js

--- src/components/ItemListContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ItemListContainer = ({greetings}) => {
-    return <>
-        <h1 style={{marginTop: "20%", color: "black"}}>{greetings}</h1>          
-    </>;
-}
-
-export default ItemListContainer
