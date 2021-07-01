ab6940ab0ad31d942285e3f264dbd888620e324b
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-07-01 02:14:34 -0300

    Delete src directory

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-const Button = ({color, backgroundColor, width, text, height, onClick, border, display}) => {
-    return(
-        <button style={{color: color, backgroundColor: backgroundColor, width: width, height: height, border: border,  borderRadius: "5px", display: display}} onClick={onClick}>{text}</button>
-    )
-}
-
-export default Button
--- src/components/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CartWidget = () => {
-    return <>
-        <a href={"/#"} id="Cart">🛒</a>          
-    </>;
-}
-
-export default CartWidget
